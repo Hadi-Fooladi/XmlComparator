3cf8d240a202aa9c5c228f82d3b453f8cc7ab5ab
Hadi Fooladi Talari <hadi.f7@gmail.com>
2018-11-30 18:09:56 -0500

    New Project: Tests

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d906c07a-74a3-489d-93df-0e1134673f4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/RootOnly.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Xml;
+using HaFT.XmlComparator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+	[TestClass]
+	public class RootOnly
+	{
+		[TestMethod]
+		public void Same()
+		{
+			XmlDocument
+				Doc1 = new XmlDocument(),
+				Doc2 = new XmlDocument();
+
+			Doc1.LoadXml("<Config />");
+			Doc2.LoadXml("<Config />");
+
+			var C = new Comparator();
+
+			Assert.IsTrue(C.AreSame(Doc1, Doc2));
+		}
+
+		[TestMethod]
+		public void DifferentName()
+		{
+			XmlDocument
+				Doc1 = new XmlDocument(),
+				Doc2 = new XmlDocument();
+
+			Doc1.LoadXml("<Config />");
+			Doc2.LoadXml("<Settings />");
+
+			var C = new Comparator();
+
+			Assert.IsFalse(C.AreSame(Doc1, Doc2));
+		}
+
+		[TestMethod]
+		public void SameAttributes()
+		{
+			XmlDocument
+				Doc1 = new XmlDocument(),
+				Doc2 = new XmlDocument();
+
+			Doc1.LoadXml("<Config a='4' b='Hello' />");
+			Doc2.LoadXml("<Config b='Hello' a='4' />");
+
+			var C = new Comparator();
+
+			Assert.IsTrue(C.AreSame(Doc1, Doc2));
+		}
+
+		[TestMethod]
+		public void DifferentNumberOfAttributes()
+		{
+			XmlDocument
+				Doc1 = new XmlDocument(),
+				Doc2 = new XmlDocument();
+
+			Doc1.LoadXml("<Config a='4' b='Hello' c='12' />");
+			Doc2.LoadXml("<Config b='Hello' a='4' />");
+
+			var C = new Comparator();
+
+			Assert.IsFalse(C.AreSame(Doc1, Doc2));
+		}
+
+		[TestMethod]
+		public void AttributesWithDifferentValue()
+		{
+			XmlDocument
+				Doc1 = new XmlDocument(),
+				Doc2 = new XmlDocument();
+
+			Doc1.LoadXml("<Config a='5' b='Hello' />");
+			Doc2.LoadXml("<Config b='Hello' a='4' />");
+
+			var C = new Comparator();
+
+			Assert.IsFalse(C.AreSame(Doc1, Doc2));
+		}
+	}
+}
