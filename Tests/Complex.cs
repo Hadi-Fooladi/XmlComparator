845dab77702714cb4027fe750b8aa879db3afd18
Hadi Fooladi Talari <hadi.f7@gmail.com>
2018-12-03 11:11:30 -0500

    Tests (v3)

--- Tests/Complex.cs
+++ Tests/Complex.cs
@@ -10,26 +10,38 @@ namespace Tests
 	[TestClass]
 	public class Complex
 	{
-		private const string ROOT = "Files\\Complex\\";
+		private const string
+			ROOT = "Files\\Complex\\",
+			FILENAME_1 = "Doc1.xml",
+			FILENAME_2 = "Doc2.xml";
+
+		private static XmlDocument LoadXmlDocument(string Filename, [CallerMemberName] string MethodName = null)
+		{
+			var Doc = new XmlDocument();
+			Doc.Load($@"{ROOT}{MethodName}\{Filename}");
+			return Doc;
+		}
 
 		private static void LoadXmlDocuments(out XmlDocument Doc1, out XmlDocument Doc2, [CallerMemberName] string MethodName = null)
 		{
-			Doc1 = new XmlDocument();
-			Doc2 = new XmlDocument();
-
-			Doc1.Load($"{ROOT}{MethodName}\\Doc1.xml");
-			Doc2.Load($"{ROOT}{MethodName}\\Doc2.xml");
+			Doc1 = LoadXmlDocument(FILENAME_1, MethodName);
+			Doc2 = LoadXmlDocument(FILENAME_2, MethodName);
 		}
 
-		[TestMethod]
-		public void Sibling_Same_DifferentOrder()
+		private static void Compare(bool ExpectedResult, [CallerMemberName] string MethodName = null)
 		{
 			XmlDocument Doc1, Doc2;
-			LoadXmlDocuments(out Doc1, out Doc2);
+			LoadXmlDocuments(out Doc1, out Doc2, MethodName);
 
 			var C = new Comparator();
 
-			Assert.IsTrue(C.AreSame(Doc1, Doc2));
+			Assert.AreEqual(ExpectedResult, C.AreSame(Doc1, Doc2));
 		}
+
+		[TestMethod]
+		public void Sibling_Same_DifferentOrder() => Compare(true);
+
+		[TestMethod]
+		public void Sibling_DifferentAttributes() => Compare(false);
 	}
 }
