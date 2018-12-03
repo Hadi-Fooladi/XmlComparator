e1f2e98f63d98f2f7c1c57f1f441925331d9e127
Hadi Fooladi Talari <hadi.f7@gmail.com>
2018-12-03 00:38:46 -0500

    Tests (v2)

---
+++ Tests/Complex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using HaFT.XmlComparator;
+
+namespace Tests
+{
+	[TestClass]
+	public class Complex
+	{
+		private const string ROOT = "Files\\Complex\\";
+
+		private static void LoadXmlDocuments(out XmlDocument Doc1, out XmlDocument Doc2, [CallerMemberName] string MethodName = null)
+		{
+			Doc1 = new XmlDocument();
+			Doc2 = new XmlDocument();
+
+			Doc1.Load($"{ROOT}{MethodName}\\Doc1.xml");
+			Doc2.Load($"{ROOT}{MethodName}\\Doc2.xml");
+		}
+
+		[TestMethod]
+		public void Sibling_Same_DifferentOrder()
+		{
+			XmlDocument Doc1, Doc2;
+			LoadXmlDocuments(out Doc1, out Doc2);
+
+			var C = new Comparator();
+
+			Assert.IsTrue(C.AreSame(Doc1, Doc2));
+		}
+	}
+}
